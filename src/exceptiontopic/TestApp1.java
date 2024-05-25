54e42d36c2a7a4e19d81699a0e71200ffdc1c2bd
rahul kirpekar <rahul.kirpekar@gmail.com>
2024-05-25 18:54:57 +0530

    exception-part-2

--- src/exceptiontopic/TestApp1.java
+++ src/exceptiontopic/TestApp1.java
@@ -6,33 +6,44 @@ public class TestApp1
 {
 	public static void main(String[] args) 
 	{
-//			Scanner sc = new Scanner(System.in);
+			Scanner sc = new Scanner(System.in);
 		
-//			System.out.println("Statement------1");
-//			System.out.println("Statement------2");
-//			System.out.println("Statement------3");
-//			System.out.println("Statement------4");
-//			System.out.println("Statement------5");
+			System.out.println("Statement------1");
+			System.out.println("Statement------2");
+			System.out.println("Statement------3");
+			System.out.println("Statement------4");
+			System.out.println("Statement------5");
 			
-//			System.out.println("Enter No1 : ");
-//			int no1 = sc.nextInt();
-//			System.out.println("Enter No2 : ");
-//			int no2 = sc.nextInt();
+			System.out.println("Enter No1 : ");
+			int no1 = sc.nextInt();
+			System.out.println("Enter No2 : ");
+			int no2 = sc.nextInt();
 			
 //				no1 / no2----class---bind----ArithmeticException(class)--jvm--object
 //										     --------------
 //											   |
 //										       "code-crashed"		
 			
-//			int ans = no1 / no2;
+			int ans = 0;
+					
+			try 
+			{
+				ans = no1 / no2;// JVM---AE--create object----assign to e(reference variable)
+				
+			} catch (ArithmeticException e) 
+			{
+				System.out.println("Catch Block Handled ---Exception");
+				e.printStackTrace();
+			}
 			
-//			System.out.println("Ans : "+ans);
+			System.out.println("Ans : "+ans);
 			
-//			System.out.println("Statement------6 ");
-//			System.out.println("Statement------7 ");
-//			System.out.println("Statement------8 ");
-//			System.out.println("Statement------9 ");
-//			System.out.println("Statement------10");
+			
+			System.out.println("Statement------6 ");
+			System.out.println("Statement------7 ");
+			System.out.println("Statement------8 ");
+			System.out.println("Statement------9 ");
+			System.out.println("Statement------10");
 			
 			
 
@@ -48,10 +59,10 @@ public class TestApp1
 //		System.out.println(name.charAt(name.length()));
 		
 		
-		String value = "3243dsf3245";
+//		String value = "3243dsf3245";
 
 		  // NumberFormateException	
-		int no1 = Integer.parseInt(value);//123
+//		int no1 = Integer.parseInt(value);//123
 		
 		
 	}
