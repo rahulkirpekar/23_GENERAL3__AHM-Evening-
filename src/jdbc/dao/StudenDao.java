e79bcccb29d59117f287511659a1467a5134eabb
rahul kirpekar <rahul.kirpekar@gmail.com>
2024-08-18 14:51:13 +0530

    jdbc-insert

--- src/jdbc/dao/StudenDao.java
+++ src/jdbc/dao/StudenDao.java
@@ -2,6 +2,7 @@ package jdbc.dao;
 
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.Scanner;
 
 import jdbc.util.DBConnection;
 
@@ -9,12 +10,13 @@ import jdbc.util.DBConnection;
 public class StudenDao 
 {
 	// insert SQL Query---Student
-	public int insertStudent() 
+	public int insertStudent(String name,String std,int marks) 
 	{
 		int rowsAffected = 0;
 		
-		String insertQuery = "INSERT INTO student(name,std,marks) VALUES ('sagar','12',100)";
+		String insertQuery = "INSERT INTO student(name,std,marks) VALUES ('"+name+"','"+std+"',"+marks+")";
 		
+		System.out.println("insertQuery : " + insertQuery);
 		Statement stmt = null;
 		
 		// 1) Database Connection Object
@@ -46,24 +48,31 @@ public class StudenDao
 	{
 
 	}
-	
 	// delete SQL Query---Student
 	public void deleteStudent() 
 	{
 
 	}
-	
 	// Select SQL Query---Student
 	public void getAllStudents() 
 	{
 
 	}
-	
 	public static void main(String[] args) 
 	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Name : ");
+		String name = sc.nextLine();
+		System.out.println("Enter Std : ");
+		String std = sc.nextLine();
+		System.out.println("Enter Marks : ");
+		int marks = sc.nextInt();
+		
+		
+		
 		StudenDao dao = new StudenDao();
 		
-		int rowsAffected = dao.insertStudent();
+		int rowsAffected = dao.insertStudent(name,std,marks);
 		
 		if (rowsAffected > 0) 
 		{
